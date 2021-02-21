91c38a766e703c5d258ce41575a71b1c9b2c4b80
kritbovorn <ktaweeyossak@yahoo.com>
2021-02-21 23:04:08 +0700

    11.Directive

---
+++ src/app/lab4/lab4.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab4',
+  templateUrl: './lab4.component.html',
+  styleUrls: ['./lab4.component.css']
+})
+export class Lab4Component implements OnInit {
+
+  labs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];   // Lab4 [3]
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
