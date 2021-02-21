4b13b7d736b068d30238615049b0d8269a1aa544
kritbovorn <ktaweeyossak@yahoo.com>
2021-02-21 19:27:21 +0700

    Finish HomeComponent

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  @Input()
+
+  description: string = "";
+  count: number = 0
+
+  constructor() { 
+    this.description = 'HomeComponent:  Angular pretty good'
+  }
+
+  countNumber() {
+    this.count += 1;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
